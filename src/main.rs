b727c170dd4edfa8239949c75309c91c1cfffc03
nan mu <mu.nan.11@outlook.com>
2024-05-10 18:29:09 +0800

    使用tock reg直接写寄存器

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,25 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
                      // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
-use cw32f030_hal as hal;
+use reg::{ahben, analog, dir, driver, lckr, odr, opendrain, speed};
+use tock_registers::interfaces::Writeable;
+
+mod reg;
 
 #[entry]
 fn main() -> ! {
-    let dp = hal::svd::Peripherals::take().unwrap();
-    //dp.SYSCTRL.hsi.write(|w| w.div().variant(0b0101));
-    dp.SYSCTRL.ahben.write(|w| w.gpioc().bit(true)); //打开ahb桥上时钟，gpioc在ahb上，1为打开
-    dp.GPIOC.lock.write(|w| w.pin13().bit(false)); //打开gpioc寄存器操作锁，1为锁，0为开
-    dp.GPIOC.analog.write(|w| w.pin13().bit(false)); //设置pc13为数字，0为数字，1为模拟
-    dp.GPIOC.dir.write(|w| w.pin13().bit(false)); //设置pc13为输出，0为输出，1为输入
-    dp.GPIOC.opendrain.write(|w| w.pin13().bit(false)); //设置pc13输出模式，0为推挽输出，1为开漏输出
-    dp.GPIOC.driver.write(|w| w.pin13().bit(false)); //设置pc13输出驱动能力。0为高驱动，1为低驱动
-    dp.GPIOC.speed.write(|w| w.pin13().bit(true)); //设置pc13输出速度，0为低速，1为高速
-    dp.GPIOC.odr().write(|w| w.pin13().bit(false)); //设置pc13输出电平，0为低，1为高
+    // sysctrl 基地址0x4001 0000
+    // gpioc 基地址0x4800 0800
+    let sysctrl: &reg::Sysctrl = unsafe { &*(0x4001_0000 as *const _) };
+    sysctrl.ahben.write(ahben::GPIOC::enable);
+    let gpioc: &reg::Gpioc = unsafe { &*(0x4800_0800 as *const _) };
+    gpioc.lckr.write(lckr::KEYPIN13::unlock);
+    gpioc.analog.write(analog::PIN13::digital);
+    gpioc.dir.write(dir::PIN13::output);
+    gpioc.opendrain.write(opendrain::PIN13::push_pull);
+    gpioc.driver.write(driver::PIN13::high);
+    gpioc.speed.write(speed::PIN13::low);
+    gpioc.odr.write(odr::PIN13::low);
+
     loop {}
 }
---
+++ src/reg.rs
@@ -0,0 +1,88 @@
+use tock_registers::{register_bitfields, register_structs, registers};
+
+register_structs! {
+    pub Sysctrl {
+        (0x00 => _reserved0),
+        (0x30 => pub ahben: registers::ReadWrite<u32, ahben::Register>),
+        (0x34 => @END),
+    }
+}
+
+register_structs! {
+    pub Gpioc {
+        (0x00 => pub dir: registers::ReadWrite<u32, dir::Register>),
+        (0x04 => pub opendrain: registers::ReadWrite<u32, opendrain::Register>),
+        (0x08 => pub speed: registers::ReadWrite<u32, speed::Register>),
+        (0x0c => _reserved0), //pdr
+        (0x10 => _reserved1), //pur
+        (0x14 => _reserved2), //afrh
+        (0x18 => _reserved3), //afrl
+        (0x1c => pub analog: registers::ReadWrite<u32, analog::Register>),
+        (0x20 => pub driver: registers::ReadWrite<u32, driver::Register>),
+        (0x24 => _reserved4), //riseie
+        (0x28 => _reserved5), //fallie
+        (0x2c => _reserved6), //highie
+        (0x30 => _reserved7), //lowie
+        (0x34 => _reserved8), //isr
+        (0x38 => _reserved9), //icr
+        (0x3c => pub lckr: registers::ReadWrite<u32, lckr::Register>),
+        (0x40 => _reserved10), //filter
+        // (0x40 => _reserved10), //filter
+        // (0x40 => _reserved10), //filter
+        // (0x40 => _reserved10), //filter
+        (0x50 => _reserved11), //idr
+        (0x54 => pub odr: registers::ReadWrite<u32, odr::Register>),
+        (0x58 => @END),
+    }
+}
+
+register_bitfields![u32,
+    pub ahben [
+        GPIOC OFFSET(6) NUMBITS(1) [
+            enable = 1,
+            disable = 0,
+        ],
+    ],
+    pub lckr [
+        KEYPIN13 OFFSET(0) NUMBITS(32) [
+            unlock = 0x5A5AFFFF,
+            lock = 0x0000,
+        ],
+    ],
+    pub analog [
+        PIN13 OFFSET(13) NUMBITS(1) [
+            analog = 1,
+            digital = 0,
+        ]
+    ],
+    pub driver [
+        PIN13 OFFSET(13) NUMBITS(1) [
+            high = 0,
+            low = 1,
+        ]
+    ],
+    pub dir [
+        PIN13 OFFSET(13) NUMBITS(1) [
+            input = 1,
+            output = 0,
+        ]
+    ],
+    pub opendrain [
+        PIN13 OFFSET(13) NUMBITS(1) [
+            open_drain = 1,
+            push_pull = 0,
+        ]
+    ],
+    pub speed [
+        PIN13 OFFSET(13) NUMBITS(1) [
+            low = 0,
+            high = 1,
+        ]
+    ],
+    pub odr [
+        PIN13 OFFSET(13) NUMBITS(1) [
+            high = 1,
+            low = 0,
+        ]
+    ],
+];
